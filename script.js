e6426a91fa7c3654b8340b48e1c9e94e05f5df66
abennett92 <saiford92@gmail.com>
2021-06-28 14:45:53 -0400

    Init Repo

---
+++ script.js
@@ -0,0 +1,11 @@
+function changeLogin(element) {
+    element.innerHTML = "Logout"
+}
+
+function remove(element) {
+    element.remove();
+}
+
+function alertLikes(element) {
+    alert("Ninja was liked")
+}
